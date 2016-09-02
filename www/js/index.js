3ea658467cc6fd74f1624596862ec47d7c5bcea1
AtomJump <peter@atomjump.com>
2016-09-02 14:31:44 +1200

    push test

--- www/js/index.js
+++ www/js/index.js
@@ -110,7 +110,7 @@ var app = {
         push.on('notification', function(data) {
             console.log('notification event');
             document.getElementById('aj-HTML-alert').style.display = "block";
-            document.getElementById('aj-HTML-alert').innerHTML = "<div class='inner-popup'>" + data.message + " " + JSON.stringify(data) + "</div>";
+            document.getElementById('aj-HTML-alert').innerHTML = "<div class='inner-popup'>" + data.message + " " + data.additionalData.actions[0].callback + "</div>";
             navigator.notification.alert(
                 data.message,         // message
                 null,                 // callback
