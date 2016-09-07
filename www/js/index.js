60d8d76a340db2626a1d75343916788b446925ba
AtomJump <peter@atomjump.com>
2016-09-07 12:42:14 +1200

    different msg format

--- www/js/index.js
+++ www/js/index.js
@@ -125,7 +125,7 @@ var app = {
         push.on('notification', function(data) {
             console.log('notification event');
             document.getElementById('aj-HTML-alert').style.display = "block";
-            document.getElementById('aj-HTML-alert-inner').innerHTML = "<span style='vertical-align: top; padding: 10px; padding-top:30px;' class='big-text'>AtomJump Message</span><br/><img  src='icon-Small@3x.png' style='padding 10px;'><ons-fab style='z-index: 1800;' position='top right'  onclick=\"app.closeNotifications();\"><ons-icon icon=\"md-close\" ></ons-icon></ons-fab><p>" + data.message + "<br/><br/><a href='javascript:' onclick='" + data.additionalData.info + "'>Visit Forum</a></p>";
+            document.getElementById('aj-HTML-alert-inner').innerHTML = "<span style='vertical-align: top; padding: 10px; padding-top:30px;' class='big-text'>AtomJump Message</span><br/><img  src='icon-Small@3x.png' style='padding 10px;'><ons-fab style='z-index: 1800;' position='top right'  onclick=\"app.closeNotifications();\"><ons-icon icon=\"md-close\" ></ons-icon></ons-fab><p>" + data.message + "<br/><br/><a href='javascript:' onclick='window.open(\"" + data.additionalData.observeUrl + "\", \"_system\")'>" + data.additionalData.observeMessage + "</a><br/><a href='javascript:' onclick='window.open(\"" + data.additionalData.removeUrl + "\", \"_system\")'>" + data.additionalData.removeMessage + "</a><br/>" + data.additionalData.forumMessage + ":" + data.additionalData.forumName  +"</p>";
             
            
        });
