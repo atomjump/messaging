68ef28c7569f602b7649d9e7939b7f8ddfec2a6b
AtomJump <peter@atomjump.com>
2021-02-28 07:15:29 +1300

    Full app option

--- www/js/index.js
+++ www/js/index.js
@@ -567,7 +567,7 @@ var app = {
   		errorThis.pull = false;
   	
   		if(typeof(PushNotification) == 'undefined') { 
-			if(confirm("Sorry, this site is configured to send system notifications in the background. For this you will need the full app from the app-store. Would you like to install this now?") {
+			if(confirm("Sorry, this site is configured to send system notifications in the background. For this you will need the full app from the app-store. Would you like to install this now?")) {
     			//Assume iOS at this stage.
     			window.open("https://itunes.apple.com/us/app/atomjump-messaging/id1153387200?ls=1&mt=8", "_blank");
     			
