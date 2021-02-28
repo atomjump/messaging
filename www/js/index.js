db90bbe39e0dbb8134cd1b1f734f6f0394badfa7
AtomJump <peter@atomjump.com>
2021-03-01 11:50:57 +1300

    create link and click it dynaimcally for dropdown on ios

--- www/js/index.js
+++ www/js/index.js
@@ -900,8 +900,9 @@ var app = {
 	
 		$("#click-url").show();
 		$("#click-url").attr("href", myUrl);
+		$("#click-url span").focus();				//TESTING
 		$("#click-url span").trigger("click");
-		$("#click-url span").trigger("click");		//iOS Safari needs a double click to work for some weird reason. This seems to work on Android Chrome too.
+		//$("#click-url span").trigger("click");		//TESTINGOUT iOS Safari needs a double click to work for some weird reason. This seems to work on Android Chrome too.
 		$("#click-url").hide();
 		
 		return;
