50fea2fc343b5892adc7a11b78a0ce4868648499
AtomJump <peter@atomjump.com>
2021-03-30 09:09:15 +1300

    testing link

--- www/js/index.js
+++ www/js/index.js
@@ -99,7 +99,7 @@ var app = {
 		console.log('notification event');
 		var finalData = {};
 		
-		innerThis = this;  //TESTING
+		var innerThis = this;  //TESTING
 		
 		 
 		//See https://github.com/phonegap/phonegap-plugin-push/blob/master/docs/API.md
