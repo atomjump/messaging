cf1474c49d2f34bd6a2b61491ba9b2a45f723f47
AtomJump <peter@atomjump.com>
2021-03-31 09:42:06 +1300

    always have correct app ref

--- www/js/index.js
+++ www/js/index.js
@@ -351,9 +351,16 @@ var app = {
         });
 
         push.on('notification', function(data) {
-           
-            app.onNotificationEvent(data, app);
- 
+            
+            if(app && app.getPlatform) {
+            	app.onNotificationEvent(data, app);
+ 			} else {
+ 				if(innerThis && innerThis.getPlatform) {
+ 					innerThis.onNotificationEvent(data, innerThis);
+ 				} else {
+ 					this.onNotificationEvent(data, this);
+ 				}
+ 			}
             
 
             push.finish(function() {
